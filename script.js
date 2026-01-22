7be5f76a9077735fc9533a2a65ac792dd3716f84
jagruti-potdar <jagrutipotdar1808@gmail.com>
2026-01-22 07:16:18 +0530

    Update script.js
    Update QR code to live site

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const websiteLink = "https://example.com";
+const websiteLink = "https://jagruti-potdar.github.io/virtual-business-card/";
 
 new QRCode(document.getElementById("qrcode"), {
     text: websiteLink,
     width: 100,
     height: 100
 });
+
